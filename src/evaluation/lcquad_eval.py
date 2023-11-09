2631f712885b8d561d581344d86ec68d99b2f11c
Reham-Osama <reham_osama93@yahoo.com>
2023-11-09 03:29:57 +0000

    Fixed missing linking time for lcquad

--- src/evaluation/lcquad_eval.py
+++ src/evaluation/lcquad_eval.py
@@ -115,6 +115,7 @@ if __name__ == '__main__':
         et = time.time()
         total_time = total_time + (et - st)
         total_understanding_time = total_understanding_time + understanding_time
+        total_linking_time = total_linking_time + linking_time
         if execution_time < 100:
             total_execution_time = total_execution_time + execution_time
             total_query_selection_time = total_query_selection_time + query_selection_time
